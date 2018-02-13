0fb6808bfe1aaf5aea51326a3a15bf769653073c
Pavel Kalian <pavel@kalian.cz>
2018-02-13 17:24:39 -0300

    Update imgkap formula

--- imgkap.rb
+++ imgkap.rb
@@ -1,8 +1,8 @@
 class Imgkap < Formula
   desc "Tool to create nautical charts in KAP format."
   homepage "https://github.com/nohal/imgkap"
-  url "https://github.com/nohal/imgkap/archive/v1.13.tar.gz"
-  sha256 "54f01060057c85467a1f198abc397a9dbc96107c8d0084e94b29552bdda44fb6"
+  url "https://github.com/nohal/imgkap/archive/v1.15.tar.gz"
+  sha256 "a9db2e8d76e59438e07dccb8c68bfe165f40af7c5ed1a7162268961762b64bed"
 
   depends_on "freeimage"
 
