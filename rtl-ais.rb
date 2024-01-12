7de9e4078aa613a032f552feaa4b0bd6e15aee9d
Pavel Kalian <pavel@kalian.cz>
2024-01-12 12:59:10 -0300

    Update rtl-ais

--- rtl-ais.rb
+++ rtl-ais.rb
@@ -1,8 +1,8 @@
 class RtlAis < Formula
   desc "Simple AIS tuner and generic dual-frequency FM demodulator"
   homepage "https://github.com/dgiardini/rtl-ais"
-  url "https://github.com/nohal/rtl-ais/archive/v0.1.20160814.tar.gz"
-  sha256 "ce646f97c921ff106639d8ed79b7b46ecbbf790fb32b975bfcb4c4790a3a0b0f"
+  url "https://github.com/nohal/rtl-ais/archive/refs/tags/v0.3.20240112.tar.gz"
+  sha256 "95884a326202fe0f98e8578b12cae13bc31b332d31e649106ebdb1e1888a2337"
 
   depends_on "librtlsdr"
   depends_on "libusb"
